03250ef93836f502a0b7fcd2f51f3c85f9ca8856
MatthewChildress <matt.t.childress@gmail.com>
2022-05-17 23:54:01 -0400

    re-uploading after attempting to make a folder system in git. javascript comments show earlier commit intentions

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+using getTime() returns milliseconds since 1-1-1970.
+need to use Date functions instead
+*/
+
+setInterval(theTime, 1000);
+// setInterval(function,y) takes a function and updates by millisecond. 1000 = 1 second. setTimeout doesn't work for set up.
+function theTime() {
+    // When called as a constructor, returns a new Date object.
+    const time = new Date();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // don't forget to make as strings
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+    let wd = days[time.getDay()];
+    let d = time.getDate();
+    let mo = time.getMonth() + 1; //Months are in array. May shows at 4 so +1 needed.
+    let y = time.getFullYear();
+    // .getHours(), .getMinutes(), .getSeconds() are part of Date.prototype
+    hours = checkTime(h);
+    mins = checkTime(m);
+    secs = checkTime(s);
+    // template literals needed else it returns undefined
+    document.getElementById("clock").textContent = `${hours} : ${mins} : ${secs}`;
+
+
+    document.getElementById("date").textContent = `${wd}, ${d} - ${mo} - ${y}`;
+}
+
+// don't forget to add i
+function checkTime(i) {
+    // adds 0 to hours,minutes, seconds 
+    if (i < 10) {
+        i = "0" + i
+    };
+    // if i isnt' returned, it'll returned undefined from a string and int being added
+    return i;
+}
+
+theTime();
